5de8f9c33aaada3decd7f42bf85904948757b627
System Administrator <yvan.venumiere@gmail.com>
2019-02-19 23:53:29 -0400

    update

--- src/AppBundle/Helpers/CustomQueries.php
+++ src/AppBundle/Helpers/CustomQueries.php
@@ -49,21 +49,47 @@ class CustomQueries
             $medias=$ormManager->doQuery('media_template',"*","LEFT JOIN medias ON medias.idmedias=media_template.medias_idmedias WHERE media_template.w_templates_idw_templates=:id",array(':id'=>$id));
 
             $retour['medias']=$medias;
-            $prices=$ormManager->doQuery('prices',"*","WHERE prices.w_templates_idw_templates=:id",array(':id'=>$id));
+            $prices=$ormManager->doQuery('prices',"*","LEFT JOIN cat_prices ON cat_prices.idcat_prices=prices.cat_prices_idcat_prices WHERE prices.w_templates_idw_templates=:id",array(':id'=>$id));
 
             //var_dump($prices);exit;
             //$options=$ormManager->doQuery('options_cat',"*","LEFT JOIN cat_prices ON cat_prices.idcat_prices=options_cat.cat_prices_idcat_prices LEFT JOIN prices ON prices.cat_prices_idcat_prices=cat_prices.idcat_prices WHERE prices.w_templates_idw_templates=:id");
 
-            $pcTab=array('GRATOS','STARTER','PREMIUM','PRO','VIP');
-            $optTab=array("ADPUB","FOOTER_COPYRIGHT","CUSTOM_DOMAIN","SUPPORT_MAIL_24","SUPPORT_PREMIUM","PRESTA_LIKE");
-
-            /*foreach($options as $opt)
-            {
-
-            }*/
-
+            $options=$ormManager->doQuery('options_cat',"*","LEFT JOIN cat_prices ON cat_prices.idcat_prices=options_cat.cat_prices_idcat_prices LEFT JOIN prices ON prices.cat_prices_idcat_prices=cat_prices.idcat_prices LEFT JOIN options ON options.idoptions=options_cat.options_idoptions WHERE prices.w_templates_idw_templates=:id AND options.opt_is_visible=1",array(':id'=>$id));
             //echo "<pre>";var_dump($options);exit;
-            $retour['prix']=$prices;
+            //$pcTab=array('GRATOS','STARTER','PREMIUM','PRO','VIP');
+            //$optTab=array("ADPUB","FOOTER_COPYRIGHT","CUSTOM_DOMAIN","SUPPORT_MAIL_24","SUPPORT_PREMIUM","PRESTA_LIKE");
+
+            $arrayPacks=array();
+            $corresPacksLabels=array();
+            $corresCoolOpt=array();
+            foreach($options as $opt)
+            {
+                if(!in_array($opt['cat_price_code'],array_keys($arrayPacks)))
+                {
+                   $arrayPacks[$opt['cat_price_code']]=array();
+                   $corresPacksLabels[$opt['cat_price_code']]=$opt['cat_price_label'];
+                }
+                $arrayPacks[$opt['cat_price_code']][$opt['opt_code']]=array
+                (
+                    "enabled"=>$opt['optcat_is_enabled'],
+                    "label"=>$opt['opt_label']
+                );
+            }
+
+            $arrayPrices=array();
+            foreach($prices as $prix)
+            {
+                if(!in_array($prix['cat_price_code'],array_keys($arrayPrices)))
+                {
+                    $arrayPrices[$prix['cat_price_code']]=$prix['price_value'];
+                }
+            }
+
+
+            $retour['prix']=$arrayPrices;
+            $retour['packs']=$arrayPacks;
+            $retour['corresPacksLabels']=$corresPacksLabels;
+
 
             return $retour;
         }
